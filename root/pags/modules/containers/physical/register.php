5e27e0b8eb60a8bcb0070d1ba0ae046f479d1509
Leonardo Scapinello <leonardo.scapinello@outlook.com>
2019-09-21 14:02:44 -0300

    feature: physical analysis

---
+++ root/pags/modules/containers/physical/register.php
@@ -0,0 +1,250 @@
+<form action="" method="POST">
+
+    <h5 class="c__title">Índice de Massa Corpórea</h5>
+
+    <div class="row">
+        <div class="col-sm-12 col-lg-12 col-xl-4">
+            <div class="widget">
+                <div class="form_input">
+                    <input autocomplete="off" type="number" id="stock_quantity" value="0" name="stock_quantity" placeholder="Quantidade encomenda" class="number">
+                    <label for="email"> <span class="floating_icon"><i class="far fa-weight"></i></span> </label>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-12 col-lg-12 col-xl-8">
+            <div class="widget">
+                <table class="table sml_tbl" style="margin: 0">
+                    <thead>
+                    <tr>
+                        <th>IMC</th>
+                        <th>Classificação OMS</th>
+                        <th>Risco de Saúde</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Menor de 18,5</td>
+                        <td>Peso abaixo do normal</td>
+                        <td>Fraqueza, fadiga, stress e/ou ansiedade</td>
+                    </tr>
+                    <tr class="odd">
+                        <td>18,5 a 25</td>
+                        <td>Normal</td>
+                        <td>Bom nível de energia, vitalidade e/ou resistência</td>
+                    </tr>
+                    <tr>
+                        <td>25 a 30</td>
+                        <td>Sobrepeso</td>
+                        <td>Fadiga, problemas digestivos e/ou circulação</td>
+                    </tr>
+                    <tr class="odd">
+                        <td>30 a 35</td>
+                        <td>Obesidade I</td>
+                        <td rowspan="2">Diabetes, hipertensão, doenças cardiacas, <br/>problemas gástricos e articulares
+                            (joelho, coluna, embolias) etc.
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>acimda de 40</td>
+                        <td>Obesidade mórbida</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
+    <h5 class="c__title">Índice de Gordura Corporal</h5>
+
+    <div class="row">
+        <div class="col-sm-12 col-lg-12 col-xl-4">
+            <div class="widget">
+                <div class="form_input">
+                    <input autocomplete="off" type="number" id="stock_quantity" value="0" name="stock_quantity" placeholder="Quantidade encomenda" class="number">
+                    <label for="email"> <span class="floating_icon"><i class="far fa-weight"></i></span> </label>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-12 col-lg-12 col-xl-8">
+            <div class="widget">
+                <table class="table sml_tbl" style="margin: 0">
+                    <thead>
+                    <tr>
+                        <th>Gênero</th>
+                        <th>Idade</th>
+                        <th>- (Baixo)</th>
+                        <th>0 (Normal)</th>
+                        <th>+ (Alto)</th>
+                        <th>++ (Muito alto)</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td rowspan="3">Mulher</td>
+                        <td>20-39</td>
+                        <td>< 21</td>
+                        <td>21,0 - 32,9</td>
+                        <td>33,0 - 38,9</td>
+                        <td>>= 39</td>
+                    </tr>
+                    <tr>
+                        <td>40-59</td>
+                        <td>< 23</td>
+                        <td>23,0 - 33,9</td>
+                        <td>34,2 - 39,9</td>
+                        <td>>= 40</td>
+                    </tr>
+                    <tr>
+                        <td>60-79</td>
+                        <td>< 24</td>
+                        <td>24,0 - 35,9</td>
+                        <td>36,0 - 41,9</td>
+                        <td>>= 42</td>
+                    </tr>
+                    <tr class="odd">
+                        <td rowspan="3">Homem</td>
+                        <td>20-39</td>
+                        <td>< 8</td>
+                        <td>8,0 - 19,9</td>
+                        <td>20,0 - 24,9</td>
+                        <td>>= 25</td>
+                    </tr>
+                    <tr class="odd">
+                        <td>40-59</td>
+                        <td>< 11</td>
+                        <td>11,0 - 21,9</td>
+                        <td>22,0 - 27,9</td>
+                        <td>>= 28</td>
+                    </tr>
+                    <tr class="odd">
+                        <td>60-79</td>
+                        <td>< 13</td>
+                        <td>13,0 - 24,9</td>
+                        <td>25,0 - 29,9</td>
+                        <td>>= 30</td>
+                    </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
+    <h5 class="c__title">Índice de Massa Muscular</h5>
+
+    <div class="row">
+        <div class="col-sm-12 col-lg-12 col-xl-4">
+            <div class="widget">
+                <div class="form_input">
+                    <input autocomplete="off" type="number" id="stock_quantity" value="0" name="stock_quantity" placeholder="Quantidade encomenda" class="number">
+                    <label for="email"> <span class="floating_icon"><i class="far fa-weight"></i></span> </label>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-12 col-lg-12 col-xl-8">
+            <div class="widget">
+                <table class="table sml_tbl" style="margin: 0">
+                    <thead>
+                    <tr>
+                        <th>Gênero</th>
+                        <th>Idade</th>
+                        <th>- (Baixo)</th>
+                        <th>0 (Normal)</th>
+                        <th>+ (Alto)</th>
+                        <th>++ (Muito alto)</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td rowspan="3">Mulher</td>
+                        <td>18-39</td>
+                        <td>< 24,3</td>
+                        <td>24,3 - 30,3</td>
+                        <td>30,4 - 35,3</td>
+                        <td>>= 35,4</td>
+                    </tr>
+                    <tr>
+                        <td>40-59</td>
+                        <td>< 24,1</td>
+                        <td>24,1 - 30,1</td>
+                        <td>30,2 - 35,1</td>
+                        <td>>= 35,2</td>
+                    </tr>
+                    <tr>
+                        <td>60-80</td>
+                        <td>< 23,9</td>
+                        <td>23,9 - 29,9</td>
+                        <td>30,0 - 34,9</td>
+                        <td>>= 35,2</td>
+                    </tr>
+                    <tr class="odd">
+                        <td rowspan="3">Homem</td>
+                        <td>18-39</td>
+                        <td>33,3</td>
+                        <td>33,3 - 39,3</td>
+                        <td>39,4 - 44,0</td>
+                        <td>>= 44,1</td>
+                    </tr>
+                    <tr class="odd">
+                        <td>40-59</td>
+                        <td>< 33,1</td>
+                        <td>33,1 - 39,1</td>
+                        <td>,6,2 - 43,8</td>
+                        <td>>= 43,9</td>
+                    </tr>
+                    <tr class="odd">
+                        <td>60-79</td>
+                        <td>< 32,9</td>
+                        <td>32,9</td>
+                        <td>39,2 - 43,8</td>
+                        <td>>= 43,7</td>
+                    </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
+    <h5 class="c__title">Índice de Gordura Visceral</h5>
+
+    <div class="row">
+        <div class="col-sm-12 col-lg-12 col-xl-4">
+            <div class="widget">
+                <div class="form_input">
+                    <input autocomplete="off" type="number" id="stock_quantity" value="0" name="stock_quantity" placeholder="Quantidade encomenda" class="number">
+                    <label for="email"> <span class="floating_icon"><i class="far fa-weight"></i></span> </label>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-12 col-lg-12 col-xl-8">
+            <div class="widget">
+                <table class="table sml_tbl" style="margin: 0">
+                    <thead>
+                    <tr>
+                        <th>01-02</th>
+                        <th>03-04</th>
+                        <th>05-06</th>
+                        <th>07-09</th>
+                        <th>10-14</th>
+                        <th>Maior que 14</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Ideal</td>
+                        <td>Normal</td>
+                        <td>Médio</td>
+                        <td>Alto</td>
+                        <td>Muito Alto<br/> (perigo a saúde)</td>
+                        <td>Altissimo <br/>(muito risco a saúde)</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</form>
